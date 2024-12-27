7b1a16f8a9267f33d5be826cd8c173b611bc4332
aldls <jealswl04@gmail.com>
2024-12-27 22:21:59 +0900

    revise4

--- app/src/main/java/com/example/threetapapp/ui/home/Contactinfo.kt
+++
@@ -1,4 +0,0 @@
-package com.example.threetapapp.ui.home
-
-data class Contactinfo (val name: String, val phonenum: String, val email: String,val photo: String)
-//전화번호
---
+++ app/src/main/java/com/example/threetapapp/ui/home/Productinfo.kt
@@ -0,0 +1,4 @@
+package com.example.threetapapp.ui.home
+
+data class Wishlistinfo (val product: String, val price: Int, val email: String,val photo: String)
+//전화번호
