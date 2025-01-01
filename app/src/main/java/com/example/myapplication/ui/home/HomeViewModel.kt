b96d3744dc9cf3bcfbd7de759d84fe91bdbd939d
aldls <jealswl04@gmail.com>
2025-01-01 14:07:56 +0900

    merge완료

--- app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
@@ -4,21 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SharedViewModel : ViewModel() {
-    private val _productList = MutableLiveData<MutableList<ProductInfo>>().apply {
-        value = mutableListOf(
-            ProductInfo("Red Shoes", "11,000", "www.naver.com", "humanicon"),
-            ProductInfo("Blue Shoes", "12,000", "www.naver.com", "humanicon"),
-            ProductInfo("Red Pants", "43,000", "www.naver.com", "humanicon"),
-            ProductInfo("Jacket", "11,000", "www.naver.com", "humanicon"),
-            ProductInfo("Pink Shorts", "16,000", "www.naver.com", "humanicon")
-        )
-    }
-    val productList: LiveData<MutableList<ProductInfo>> get() = _productList
+class HomeViewModel : ViewModel() {
 
-    fun addProduct(product: ProductInfo) {
-        val updatedList = _productList.value ?: mutableListOf()
-        updatedList.add(product)
-        _productList.value = updatedList
+    private val _text = MutableLiveData<String>().apply {
+        value = "Welcome Home"
     }
+    val text: LiveData<String> = _text
 }
